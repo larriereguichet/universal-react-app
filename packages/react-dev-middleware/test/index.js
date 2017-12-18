59b6829b6212fa93fbf126edf3101c9356a94aea
lutangar <johan.dufour@gmail.com>
2017-12-18 17:28:09 +0100

    remove lint and test related configuration from react-dev-middleware that are now handled by the global package

--- packages/react-dev-middleware/test/index.js
+++ packages/react-dev-middleware/test/index.js
@@ -4,23 +4,25 @@ import createDevMiddleware, { createConfig } from '../lib/index.js';
 const defaultConfig = { entry: './index.js' };
 const configCreator = env => defaultConfig;
 
-describe('create config', () => {
-  describe('should returns an object', () => {
-    it('when fed with a function', () => {
-      const config = createConfig(configCreator);
-      expect(typeof config).toBe('object');
-    });
+describe('react-dev-middleware', () => {
+  describe('create config', () => {
+    describe('should returns an object', () => {
+      it('when fed with a function', () => {
+        const config = createConfig(configCreator);
+        expect(typeof config).toBe('object');
+      });
 
-    it('when fed with an object', () => {
-      const config = createConfig(defaultConfig);
-      expect(typeof config).toBe('object');
+      it('when fed with an object', () => {
+        const config = createConfig(defaultConfig);
+        expect(typeof config).toBe('object');
+      });
     });
   });
-});
 
-describe('create dev middleware', () => {
-  it('should returns an array of middlewares', () => {
-    const middleware = createDevMiddleware(configCreator);
-    expect(middleware).toHaveLength(2);
+  describe('create dev middleware', () => {
+    it('should returns an array of middlewares', () => {
+      const middleware = createDevMiddleware(configCreator);
+      expect(middleware).toHaveLength(2);
+    });
   });
 });
