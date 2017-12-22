7262df73f9b5ccfbc925abd3cc2e0b7fc076a0d9
lutangar <johan.dufour@gmail.com>
2017-12-22 18:52:47 +0100

    [babel-preset] add export namespace from proposal

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -17,6 +17,7 @@ module.exports = function() {
       require('@babel/plugin-proposal-class-properties'),
       require('@babel/plugin-proposal-object-rest-spread'),
       require('@babel/plugin-proposal-export-default-from'),
+      require('@babel/plugin-proposal-export-namespace-from'),
     ],
   };
 };
