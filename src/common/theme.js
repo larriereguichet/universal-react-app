277c4369bdb3821f16211f53cfbeb9147818a184
lutangar <johan.dufour@gmail.com>
2018-01-15 15:09:53 +0100

    fix theme after material-ui breaking change...

--- src/common/theme.js
+++ src/common/theme.js
@@ -3,8 +3,8 @@ import { green, red } from 'material-ui/colors';
 
 const theme = createMuiTheme({
   palette: {
-    primary: green,
-    accent: red,
+    primary: { light: green[300], main: green[500], dark: green[700] },
+    accent: { light: red[300], main: red[500], dark: red[700] },
     type: 'light',
   },
 });
